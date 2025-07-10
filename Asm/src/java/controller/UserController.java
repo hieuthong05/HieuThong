066908b441dd312a413932b8440fedfedf5b8cc6
phatom2005 <khoinguyen.vo910@gmail.com>
2025-07-10 15:56:42 +0700

    image 10/20

--- Asm/src/java/controller/UserController.java
+++ Asm/src/java/controller/UserController.java
@@ -142,6 +142,8 @@ public class UserController extends HttpServlet {
         String fullName = request.getParameter("fullName");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        
+        return "";
     }
 
 }
